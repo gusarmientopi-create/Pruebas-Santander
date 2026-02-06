de3a05065c21a4d50697a91754efef53909145d8
guillermo <memos200101@gmail.com>
2026-02-06 10:02:24 -0500

    eliminar documento

--- src/main/java/com/banco/apitarjetas/service/DescuentoServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.banco.apitarjetas.service;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-@ExtendWith(MokitoExtension.class)
-public class DescuentoServiceTest {
-
-}
